08a5d2345459a405a891a04693aa66f8b4daa752
karolkocis <114649766+karolkocis@users.noreply.github.com>
2022-11-21 17:20:27 +0100

    test

--- config.php
+++ config.php
@@ -2,10 +2,16 @@
     extract($_REQUEST);
     $file=fopen("form-save.txt","a");
 
-    fwrite($file,"name :");
+    fwrite($file,"Name :");
     fwrite($file, $username ."\n");
     fwrite($file,"Email :");
     fwrite($file, $email ."\n");
+    fwrite($file,"Phone number :");
+    fwrite($file, $phonenumber ."\n");
+    fwrite($file,"Adress :");
+    fwrite($file, $adress ."\n");
+    fwrite($file,"Gender :");
+    fwrite($file, $gender ."\n");
     fwrite($file,"Password :");
     fwrite($file, $password ."\n");
     fclose($file);
