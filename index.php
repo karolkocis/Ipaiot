4d77bfa49e9444da08726054340923f8daf3b5a3
karolkocis <114649766+karolkocis@users.noreply.github.com>
2023-01-19 13:33:23 +0100

    oprava3

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 $numbers = array(4, 10, 8, 15, 1, 0, 6, 8 ,19, 20, 3);
-print_r($numbers);
+foreach($numbers as $item){
+  echo $item . "\n";
+}
 
 $largest = max($numbers);
 print_r("Najvacsia hodnota: ".$largest."\n");
