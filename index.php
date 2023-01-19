b71bed530fb1512208f67008b234e19caa1cf477
karolkocis <114649766+karolkocis@users.noreply.github.com>
2023-01-19 13:51:05 +0100

    style

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<style type="text/css">
+    body,td,th {
+    font-size: 14px;
+    color: #FFFFFF;
+    text-align: center;
+}
+</style>
 <?php
 $numbers = array(4, 10, 8, 15, 1, 0, 6, 8 ,19, 20, 3);
 print_r("Pole: ");
