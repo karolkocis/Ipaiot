a708717cce7f4b8cb52d7d96bc50de644dbace4a
karolkocis <114649766+karolkocis@users.noreply.github.com>
2023-01-19 13:29:42 +0100

    oprava2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $numbers = array(4, 10, 8, 15, 1, 0, 6, 8 ,19, 20, 3);
-print_r($numbers."\n");
+print_r($numbers);
 
 $largest = max($numbers);
 print_r("Najvacsia hodnota: ".$largest."\n");
