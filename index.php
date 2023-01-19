50b0fa2339b621de0db4ca86a66d099479078b22
karolkocis <114649766+karolkocis@users.noreply.github.com>
2023-01-19 13:28:04 +0100

    orpava1

--- index.php
+++ index.php
@@ -3,10 +3,12 @@ $numbers = array(4, 10, 8, 15, 1, 0, 6, 8 ,19, 20, 3);
 print_r($numbers."\n");
 
 $largest = max($numbers);
-print_r("Najvacsia hodnota: "$largest."\n");
+print_r("Najvacsia hodnota: ".$largest."\n");
 
 $smallest = min($numbers);
-print_r("Najmensia hodnota: "$smallest."\n");
+print_r("Najmensia hodnota: ".$smallest."\n");
 
+$sorted = asort($numbers);
+print_r("Zoradene pole: ".$sorted);
 
 ?>
